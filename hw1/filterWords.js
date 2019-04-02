b69f229fa3269fc4b32abf2cfe958ce3f4a4fb3d
Munkhzul Khurelbaatar <julia@Munkhzuls-MacBook-Pro.local>
2019-04-01 22:09:07 -0500

    initial commit

---
+++ hw1/filterWords.js
@@ -0,0 +1,33 @@
+const { from } = require("rxjs");
+// String.prototype.filterWords = function(words) {
+//   let s = this;
+//   for (let val of words) {
+//     s = s.replace(val, "***");
+//   }
+//   return s;
+// };
+String.prototype.filterWords = function(words) {
+  let reg = new RegExp(words.join("|"), "gi");
+  return this.replace(reg, "***");
+};
+const promise = function() {
+  return new Promise(function(resolve) {
+    resolve("This house is nice!".filterWords(["house", "nice"]));
+  });
+};
+async function aSync() {
+  try {
+    let result = await promise();
+    console.log(result);
+  } catch {
+    console.log("error");
+  }
+}
+from(promise).subscribe(e => console.log(e));
+console.log("observable finished");
+console.log("This house is nice!".filterWords(["house", "nice"]));
+console.log("ES6 finished");
+promise().then(data => console.log(data));
+console.log("Promise finished");
+aSync();
+console.log("async await finished");
