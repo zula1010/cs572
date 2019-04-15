442cbe7a9c2478d7f451c61baf23fc71c379c0dd
Munkhzul Khurelbaatar <julia@Munkhzuls-MacBook-Pro.local>
2019-04-14 23:38:32 -0500

    homeowork 10 done

---
+++ homework_10/typescript.js
@@ -0,0 +1,12 @@
+var University = (function () {
+    function University(name, dept) {
+        this.name = name;
+        this.dept = dept;
+    }
+    University.prototype.graduation = function (year) {
+        console.log("Graduating {$this.dept} {$year} students");
+    };
+    return University;
+})();
+var mum = new University("MUM", "Computer Science");
+mum.graduation(2019);
